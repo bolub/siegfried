f49a72771e88b8c53adf8da88a046f568d50f93a
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-31 01:36:06 +0300

    Use logo

--- src/containers/Home/Navbar.tsx
+++ src/containers/Home/Navbar.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import { Logo } from "@/components/Logo";
 
 export const Navbar = () => {
   return (
     <nav className="container mx-auto flex h-[94px] w-full items-center">
-      <span className="font-mono text-base font-bold uppercase text-primary-100">
-        Siegfried
-      </span>
+      <Logo />
     </nav>
   );
 };
