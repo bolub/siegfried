a15bc6b9c8c83ae9fbac105a737791e1fe039931
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-30 21:58:01 +0300

    Update theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,20 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        100: "#101828",
+      },
+      placeholder: {
+        100: "#667085",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-dm-sans)"],
+        mono: ["var(--font-space-mono)"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
