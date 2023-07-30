c86442a3676067ce24fcabf4f0e089433b81cc3c
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-31 01:36:25 +0300

    Create new pages

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,30 @@
+import { Appshell } from "@/components/Appshell";
+import { routes } from "@/routes";
+import { getServerAuthSession } from "@/server/auth";
+import { type GetServerSidePropsContext } from "next";
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const session = await getServerAuthSession(ctx);
+  const userId = session?.user?.id;
+
+  if (!userId) {
+    return {
+      redirect: {
+        destination: routes.login(),
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
+export default function Dashboard() {
+  return (
+    <Appshell>
+      <p>Hello from dashboard</p>
+    </Appshell>
+  );
+}
