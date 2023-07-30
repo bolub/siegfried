3b668f8d786f7b399dd3d3265599c29596921ea6
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-30 23:52:08 +0300

    Use homePage container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,5 @@
-import Head from "next/head";
-import Link from "next/link";
+import { HomePage } from "@/containers/Home/Home";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Create T3 App</title>
-        <meta name="description" content="Sign signatures" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <header className="container mx-auto py-10">
-        <h1 className="text-2xl">Welcome to siegfried signatures</h1>
-      </header>
-
-      <main>
-        <section className="container mx-auto">
-          <Link
-            className="rounded-md bg-black px-6 py-3 font-medium text-white ring-offset-2 focus:outline-none focus:ring-2 focus:ring-black"
-            href="/login"
-          >
-            Login
-          </Link>
-        </section>
-      </main>
-    </>
-  );
+  return <HomePage />;
 }
