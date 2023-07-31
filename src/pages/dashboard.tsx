7fc3f9db5a608e55795edb1b77e7cd81477162a9
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-31 08:50:54 +0300

    comment

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,21 +4,19 @@ import { getServerAuthSession } from "@/server/auth";
 import { type GetServerSidePropsContext } from "next";
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  const session = await getServerAuthSession(ctx);
-  const userId = session?.user?.id;
-
-  if (!userId) {
-    return {
-      redirect: {
-        destination: routes.login(),
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {},
-  };
+  // const session = await getServerAuthSession(ctx);
+  // const userId = session?.user?.id;
+  // if (!userId) {
+  //   return {
+  //     redirect: {
+  //       destination: routes.login(),
+  //       permanent: false,
+  //     },
+  //   };
+  // }
+  // return {
+  //   props: {},
+  // };
 };
 
 export default function Dashboard() {
