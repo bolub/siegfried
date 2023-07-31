24e5fce0abf043295790885f481004b81ff2a4d4
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-31 21:09:23 +0300

    get session data

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,30 +1,33 @@
 import { Appshell } from "@/components/Appshell";
-import { routes } from "@/routes";
-import { authOptions } from "@/server/auth";
-import { type GetServerSidePropsContext } from "next";
-import { getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
+// import { routes } from "@/routes";
+// import { authOptions } from "@/server/auth";
+// import { type GetServerSidePropsContext } from "next";
+// import { getServerSession } from "next-auth";
 
-export const getServerSideProps = async ({
-  req,
-  res,
-}: GetServerSidePropsContext) => {
-  const session = await getServerSession(req, res, authOptions);
+// export const getServerSideProps = async ({
+//   req,
+//   res,
+// }: GetServerSidePropsContext) => {
+//   const session = await getServerSession(req, res, authOptions);
 
-  if (session?.user && session.user.id) {
-    return {
-      redirect: {
-        destination: routes.dashboard(),
-        permanent: false,
-      },
-    };
-  } else {
-    return {
-      props: {},
-    };
-  }
-};
+//   if (session?.user && session.user.id) {
+//     return {
+//       redirect: {
+//         destination: routes.dashboard(),
+//         permanent: false,
+//       },
+//     };
+//   } else {
+//     return {
+//       props: {},
+//     };
+//   }
+// };
 
 export default function Dashboard() {
+  const session = useSession();
+  console.log(session);
   return (
     <Appshell>
       <p>Hello from dashboard</p>
