063f59fe75b081f68c8e7694945a64e6417201b9
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-31 21:01:09 +0300

    test again

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,27 +1,28 @@
 import { Appshell } from "@/components/Appshell";
 import { routes } from "@/routes";
-import { getServerAuthSession } from "@/server/auth";
+import { authOptions } from "@/server/auth";
 import { type GetServerSidePropsContext } from "next";
+import { getServerSession } from "next-auth";
 
-export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  const session = await getServerAuthSession(ctx);
+export const getServerSideProps = async ({
+  req,
+  res,
+}: GetServerSidePropsContext) => {
+  const session = await getServerSession(req, res, authOptions);
   const userId = session?.user?.id;
 
-  // if (!userId) {
-  //   return {
-  //     props: {},
-  //     redirect: {
-  //       destination: routes.login(),
-  //       permanent: false,
-  //     },
-  //   };
-  // }
-
-  return {
-    props: {
-      userId,
-    },
-  };
+  if (!userId) {
+    return {
+      redirect: {
+        destination: routes.dashboard(),
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      props: {},
+    };
+  }
 };
 
 export default function Dashboard() {
