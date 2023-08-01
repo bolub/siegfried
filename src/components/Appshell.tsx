2d6a5128faab5f0e4109f8f1f1dc2ade9ec3afe4
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-01 06:20:19 +0300

    Add top margin

--- src/components/Appshell.tsx
+++ src/components/Appshell.tsx
@@ -79,7 +79,7 @@ export const Appshell: FC<AppshellProps> = ({ children }) => {
         </DropdownMenu>
       </nav>
 
-      <div className="container mx-auto w-full">{children}</div>
+      <div className="container mx-auto mt-12 w-full">{children}</div>
     </>
   );
 };
