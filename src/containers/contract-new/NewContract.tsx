f202ac35d0715ad0b3fc4f041157aabd92a33b66
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-01 11:04:55 +0300

    Update path

--- src/containers/contract-new/NewContract.tsx
+++ src/containers/contract-new/NewContract.tsx
@@ -4,7 +4,7 @@
 
 import React from "react";
 import { ContractTitleEditor } from "@/containers/contract-new/components/ContractTitleEditor";
-import { ContractEditor } from "@/containers/contract-new/components/ContractEditor";
+import { ContractEditor } from "@/containers/contract-new/components/ContractEditor/ContractEditor";
 import { ContractSigners } from "@/containers/contract-new/components/ContractSigners";
 import {
   useForm,
