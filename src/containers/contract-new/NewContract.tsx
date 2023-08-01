524fdf18b4502b8f4e8a6eadc969de0f8bfd12af
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-01 06:48:57 +0300

    Create new contract page

---
+++ src/containers/contract-new/NewContract.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TopBar } from "@/containers/contract-new/components/TopBar";
+
+export const NewContractPage = () => {
+  return (
+    <div>
+      <TopBar />
+      hi
+    </div>
+  );
+};
