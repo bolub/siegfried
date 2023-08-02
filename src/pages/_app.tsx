c84107b4164c2f103b89d5e2b062348067a28930
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-02 22:02:19 +0300

    hide next progress from router page so it dosen't affect the hook to check for unsaved changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,20 @@ import { api } from "@/utils/api";
 import "@/styles/globals.css";
 import { dm_sans, space_mono } from "theme/fonts";
 import NextProgress from "next-progress";
+import { useRouter } from "next/router";
+import { routes } from "@/routes";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const router = useRouter();
+
   return (
     <SessionProvider session={session}>
-      <NextProgress height={4} color="black" />
+      {router.pathname !== routes.contracts.new() && (
+        <NextProgress height={4} color="black" />
+      )}
 
       <div className={`${dm_sans.variable} ${space_mono.variable} font-sans`}>
         <Component {...pageProps} />
