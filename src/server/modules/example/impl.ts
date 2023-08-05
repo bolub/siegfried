cefa504676573b3ddabd2d4ce5102afc1b3d3dd7
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-05 10:44:16 +0300

    Use react email contract email template to send test emails

--- src/server/modules/example/impl.ts
+++ src/server/modules/example/impl.ts
@@ -1,13 +1,23 @@
 import { transporter } from "@/server/modules/nodemailer/impl";
-import { type Example } from "./interface";
+import { render } from "@react-email/render";
+import { type Example } from "@/server/modules/example/interface";
+import Contract from "@/emails/contract";
 
 const testEmailSending: Example["testEmailSending"] = async () => {
   return await transporter
     .sendMail({
       from: "abiol5202@gmail.com",
       to: "biolaseyi19@gmail.com",
-      subject: "Hello 2",
-      text: "Hello, World!",
+      subject: "Contract email test",
+      html: render(
+        Contract({
+          contractName: "Bolu Frontend Contract",
+          contractUrl: "https://boluabiola.com",
+          user: {
+            name: "Bolu Abiola",
+          },
+        })
+      ),
     })
     .then(() => {
       return "email sent successfully";
