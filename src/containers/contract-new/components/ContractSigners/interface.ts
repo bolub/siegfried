c3dea34c7782782816772771ce17b01404c0da1a
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-05 15:21:54 +0300

    Update types

--- src/containers/contract-new/components/ContractSigners/interface.ts
+++ src/containers/contract-new/components/ContractSigners/interface.ts
@@ -1,17 +1,20 @@
 import { type Control, type UseFormRegister } from "react-hook-form";
 import { z } from "zod";
 
-export const SignerSchema = z.object({
+export const ContractSignerSchema = z.object({
   name: z.string(),
   email: z.string().email(),
 });
 
+export const ContractSignersSchema = z.array(ContractSignerSchema);
+
 export const FormDataSchema = z.object({
   contractName: z.string(),
   contractContent: z.string(),
-  signers: z.array(SignerSchema),
+  signers: z.array(ContractSignerSchema),
 });
 
+export type ContractSigners = z.infer<typeof ContractSignersSchema>;
 export type ContractFormData = z.infer<typeof FormDataSchema>;
 export type ContractFormRegisterType = UseFormRegister<ContractFormData>;
 export type ContractFormControlType = Control<ContractFormData>;
