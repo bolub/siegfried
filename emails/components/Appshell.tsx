07c0a2c017cfe5acb87d95d330874b0425991b92
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-05 08:23:17 +0300

    Create appshell for emails

---
+++ emails/components/Appshell.tsx
@@ -0,0 +1,54 @@
+{
+  /* eslint-disable @next/next/no-page-custom-font */
+}
+import { Tailwind } from "@react-email/tailwind";
+import { type ReactNode } from "react";
+import {
+  Body,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Section,
+} from "@react-email/components";
+import tailwindTheme from "./tailwind.config";
+import { EmailLogo } from "./EmailLogo";
+
+export const Appshell = ({ children }: { children: ReactNode }) => {
+  return (
+    <Tailwind
+      config={{
+        theme: tailwindTheme.theme,
+      }}
+    >
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600;9..40,700;9..40,800&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <Preview>Log in with this magic link</Preview>
+        <Body className="bg-background-100 font-sans">
+          <Section>
+            <Container className="my-auto w-full max-w-[532px] bg-white px-10 pb-16 pt-2">
+              <EmailLogo />
+
+              <div className="mt-4">{children}</div>
+            </Container>
+          </Section>
+        </Body>
+      </Html>
+    </Tailwind>
+  );
+};
