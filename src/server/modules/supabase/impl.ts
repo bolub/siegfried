49deffa97e94708d8eb4fbd45f75477c1a79b169
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-07 20:53:32 +0300

    Add supabase initialization

---
+++ src/server/modules/supabase/impl.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+import { env } from "@/env.mjs";
+import { SupabaseClient, createClient } from "@supabase/supabase-js";
+
+export const supabase =
+  // @ts-ignore
+  (global.supabase as SupabaseClient) ||
+  (env.SUPABASE_URL &&
+    env.SUPABASE_KEY &&
+    createClient(env.SUPABASE_URL, env.SUPABASE_KEY));
+
+// do we still need this?
+if (env.NODE_ENV !== "production") {
+  // @ts-ignore
+  global.supabase = supabase;
+}
