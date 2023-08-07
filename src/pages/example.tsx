00119a79078a41219c2add5eed8e0163dd560f50
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-07 20:49:35 +0300

    Add test for upload to supabase

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -6,17 +6,60 @@ export default function Example() {
   const { mutate: testEmailSending } =
     api.example.testEmailSending.useMutation();
 
+  const { mutate: testGeneratePdf, mutateAsync: testGeneratePdfAsync } =
+    api.example.testGeneratePdf.useMutation();
+
+  const sendPdfToUrlTest = async () => {
+    const { pdfFilePath } = await testGeneratePdfAsync();
+
+    // upload pdf to supabase
+    try {
+      const response = await fetch("/api/contracts/upload", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ filePath: pdfFilePath }),
+      });
+
+      console.log(await response.json());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Appshell>
-        <p>Hello from Example</p>
-        <Button
-          onClick={() => {
-            testEmailSending();
-          }}
-        >
-          Send test email
-        </Button>
+        <div className="flex flex-col gap-4">
+          <Button
+            onClick={() => {
+              testEmailSending();
+            }}
+            className="w-fit"
+          >
+            Send test email
+          </Button>
+
+          <Button
+            onClick={() => {
+              testGeneratePdf();
+            }}
+            className="w-fit"
+          >
+            Generate Pdf test
+          </Button>
+
+          <Button
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onClick={async () => {
+              await sendPdfToUrlTest();
+            }}
+            className="w-fit"
+          >
+            Generate Pdf url test
+          </Button>
+        </div>
       </Appshell>
     </>
   );
