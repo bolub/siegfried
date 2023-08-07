c70f573356d9450b4cb49f654bf3e7bf991d59a2
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-07 20:52:49 +0300

    Add Filestorage service

---
+++ src/server/modules/file-storage-service/interface.ts
@@ -0,0 +1,13 @@
+export interface FileStorageTypes {
+  upload: (args: {
+    path: string;
+    file: File | NodeJS.ReadableStream | ArrayBuffer;
+    bucket: string;
+    opts?: { contentType?: string; upsert?: boolean };
+  }) => Promise<{ path: string }>;
+  temporalUrl: (args: {
+    bucket: string;
+    path: string;
+    expiresIn?: number;
+  }) => Promise<{ url: string }>;
+}
