e7df3cbbece35129bc3571d9eb96f4bae0ec082e
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-08 20:58:20 +0300

    use lines bg

--- src/containers/home-page/Home.tsx
+++ src/containers/home-page/Home.tsx
@@ -6,27 +6,32 @@ import { routes } from "@/routes";
 
 export const HomePage = () => {
   return (
-    <>
+    <main
+      className="h-screen pt-16 md:pt-32"
+      style={{
+        backgroundImage: 'url("/lines.svg")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <Navbar />
 
-      <main className="mt-16 md:mt-32">
-        <header className="container mx-auto w-full">
-          <div className="max-w-xl">
-            <h1 className="font-mono text-[56px] font-bold leading-[60px]">
-              Create and sign documents easily
-            </h1>
-            <p className="mt-10 text-lg">
-              Powerful, self-serve product and growth analytics to help you
-              convert, engage, and retain more users. Trusted by over 4,000
-              startups.
-            </p>
+      <header className="container mx-auto w-full">
+        <div className="max-w-xl">
+          <h1 className="font-mono text-[56px] font-bold leading-[60px]">
+            Create and sign documents easily
+          </h1>
+          <p className="mt-10 text-lg">
+            Powerful, self-serve product and growth analytics to help you
+            convert, engage, and retain more users. Trusted by over 4,000
+            startups.
+          </p>
 
-            <Button asChild size="lg" className="mt-14">
-              <Link href={routes.login()}>Get Started</Link>
-            </Button>
-          </div>
-        </header>
-      </main>
-    </>
+          <Button asChild size="lg" className="mt-14">
+            <Link href={routes.login()}>Get Started</Link>
+          </Button>
+        </div>
+      </header>
+    </main>
   );
 };
