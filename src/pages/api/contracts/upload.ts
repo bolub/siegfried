6cd4f9dab64d7e1d8787d6fe0388eae89c5a4a77
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-08 16:27:28 +0300

    remove unused and fix types

--- src/pages/api/contracts/upload.ts
+++ src/pages/api/contracts/upload.ts
@@ -1,9 +1,9 @@
-import { handlePost, handlePostWithSession } from "@/lib/http";
+import { handlePost } from "@/lib/http";
 
 import { FileStorageService } from "@/server/modules/file-storage-service/impl";
 import { env } from "@/env.mjs";
 import fs from "fs";
-import { NextApiRequest, NextApiResponse } from "next";
+import { type NextApiRequest, type NextApiResponse } from "next";
 
 export const config = {
   api: {
