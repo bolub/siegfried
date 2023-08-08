a737423f3e72c7f3eabe11f58273b4ac8d8f576b
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-08 15:17:17 +0300

    use protected procedure

--- src/server/modules/contract-service/router.ts
+++ src/server/modules/contract-service/router.ts
@@ -1,10 +1,14 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
 import { ContractSignerSchema } from "@/containers/contract-new/components/ContractSigners/interface";
 import { ContractService } from "@/server/modules/contract-service/impl";
 
 export const contractServiceRouter = createTRPCRouter({
-  create: publicProcedure
+  create: protectedProcedure
     .input(
       z.object({
         contractName: z.string(),
