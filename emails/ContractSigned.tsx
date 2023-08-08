334fdcc49538ccf255e0883b44cc00a52c78d0af
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-08 15:32:30 +0300

    Create template for contract signed

---
+++ emails/ContractSigned.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Appshell } from "./components/Appshell";
+import { Text } from "@react-email/components";
+
+export const ContractSigned = ({
+  contractName = "Frontend Contract Agreement",
+}: {
+  contractName: string;
+}) => {
+  return (
+    <Appshell>
+      <Text className="text-xl font-bold">Hello,</Text>
+
+      <Text className="mt-5 text-base">
+        <span className="font-bold">{contractName}</span> has been signed
+        successfully
+      </Text>
+    </Appshell>
+  );
+};
+
+export default ContractSigned;
