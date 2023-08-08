b4d790f1a37b8cdb3c4b4e7a808febac9a0c5e61
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-08 15:17:05 +0300

    refactor

--- src/server/modules/contract-service/impl.ts
+++ src/server/modules/contract-service/impl.ts
@@ -1,129 +1,12 @@
-import Contract from "@/emails/contract";
 import { env } from "@/env.mjs";
-import { routes } from "@/routes";
 import { prisma } from "@/server/db";
 import { type ContractServiceType } from "@/server/modules/contract-service/interface";
-import { transporter } from "@/server/modules/email-service/impl";
-import {
-  type Contract as PrismaContract,
-  type ContractRecipient,
-} from "@prisma/client";
-import { render } from "@react-email/render";
-import { TokenService } from "@/server/modules/token-service/impl";
 import { PdfService } from "@/server/modules/pdf-service/impl";
-import { ContractUser } from "@/containers/contract/utils";
-import { FileStorageService } from "../file-storage-service/impl";
-
-const sendContractEmailsToSigners = ({
-  contract,
-  user,
-}: {
-  contract: PrismaContract & {
-    recipients: ContractRecipient[];
-  };
-  user: {
-    name?: string | null;
-  };
-}) => {
-  const token = TokenService.generateToken({
-    contractId: contract.id,
-  });
-
-  contract.recipients.forEach(async (signer) => {
-    await transporter.sendMail({
-      from: env.CONTACT_EMAIL,
-      to: signer.email,
-      subject: `Request to sign ${contract.name} from ${user.name}`,
-      html: render(
-        Contract({
-          contractName: contract.name,
-          contractUrl: `${env.NEXTAUTH_URL}${routes.contracts.view(
-            contract.id
-          )}?token=${token}&user=${signer.id}`,
-          user: {
-            name: user.name ?? "",
-          },
-        })
-      ),
-    });
-  });
-};
-
-const sendContractSignedEmail = async ({
-  contractId,
-  recipientId,
-  storageId,
-}: {
-  contractId: string;
-  recipientId: string;
-  storageId: string;
-}) => {
-  const contract = await prisma.contract.findUnique({
-    where: {
-      id: contractId,
-    },
-    include: {
-      user: true,
-      recipients: true,
-    },
-  });
-
-  if (!contract) {
-    throw new Error("Contract does not exist");
-  }
-
-  if (!contract.user) {
-    throw new Error("User does not exist");
-  }
-
-  if (!contract.recipients) {
-    throw new Error("Contract does not have any recipients");
-  }
-
-  const recipient = ContractUser({
-    recipients: contract.recipients,
-    userId: recipientId,
-  });
-
-  const file = await FileStorageService.download({
-    bucket: env.SUPABASE_CONTRACTS_BUCKET,
-    path: storageId,
-  });
-
-  const fileToArrayBuffer = await file.blob.arrayBuffer();
-
-  await Promise.all([
-    // send to user
-    transporter.sendMail({
-      from: env.CONTACT_EMAIL,
-      to: contract.user.email || "",
-      subject: `${recipient?.name} has signed the contract`,
-      text: "Contract signed successfully",
-      attachments: [
-        {
-          filename: storageId,
-          content: Buffer.from(fileToArrayBuffer),
-          contentType: "application/pdf",
-        },
-      ],
-    }),
-
-    // send to recipient
-    transporter.sendMail({
-      from: env.CONTACT_EMAIL,
-      to: recipient?.email,
-      subject: `${contract.name} signed successfully`,
-      text: "Contract signed successfully",
-      attachments: [
-        {
-          filename: storageId,
-          content: Buffer.from(fileToArrayBuffer),
-          contentType: "application/pdf",
-        },
-      ],
-    }),
-  ]);
-};
+import {
+  getContract,
+  sendContractEmailsToSigners,
+  sendContractSignedEmail,
+} from "@/server/modules/contract-service/utils";
 
 export const create: ContractServiceType["create"] = async (args) => {
   const { contract, user } = args;
@@ -168,9 +51,15 @@ export const signContract: ContractServiceType["signContract"] = async ({
   recipientId,
   userId,
 }) => {
-  // get our pdf from pdfService - html
+  const contract = await getContract({
+    contractId,
+  });
+
+  const pdfName = `${contract.name} signed`;
+
   const generatedPdfFilePath = await PdfService.generatePdf({
     html: contractContent,
+    name: pdfName,
   });
 
   if (!generatedPdfFilePath) {
@@ -182,12 +71,15 @@ export const signContract: ContractServiceType["signContract"] = async ({
   }
 
   // send pdf over to supabase to be saved
-  const response = await fetch("http://localhost:3000/api/contracts/upload", {
+  const response = await fetch(`${env.NEXTAUTH_URL}/api/contracts/upload`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ filePath: generatedPdfFilePath.pdfFilePath }),
+    body: JSON.stringify({
+      filePath: generatedPdfFilePath.pdfFilePath,
+      pdfName,
+    }),
   });
 
   const { data: supabaseFilePath } = (await response.json()) as {
@@ -204,9 +96,19 @@ export const signContract: ContractServiceType["signContract"] = async ({
       },
     }),
 
+    // set signed status to SIGNED
+    prisma.contract.update({
+      where: {
+        id: contractId,
+      },
+      data: {
+        status: "SIGNED",
+      },
+    }),
+
     // send emails
     sendContractSignedEmail({
-      contractId,
+      contract,
       recipientId,
       storageId: supabaseFilePath,
     }),
