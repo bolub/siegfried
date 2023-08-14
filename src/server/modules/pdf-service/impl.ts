8d8ffc0bd56dad7af604f10f817e76721324999f
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-14 09:32:25 +0300

    try with vercel path

--- src/server/modules/pdf-service/impl.ts
+++ src/server/modules/pdf-service/impl.ts
@@ -8,7 +8,7 @@ export const generatePdf: PdfServiceType["generatePdf"] = async ({
   name,
 }) => {
   const browser = await chromium.launch({
-    executablePath: "/ms-playwright/chromium-1076",
+    executablePath: "/vercel/.cache/ms-playwright/chromium-1076",
   });
   const page = await browser.newPage();
 
