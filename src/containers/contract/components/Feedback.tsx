1b3aec7d8f0380b2749930ddbc411d8bee60b309
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-15 16:02:49 +0300

    Add LoadingState component

--- src/containers/contract/components/Feedback.tsx
+++ src/containers/contract/components/Feedback.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { routes } from "@/routes";
+import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -11,6 +12,17 @@ export const NoContractDataAvailable = () => {
       <Link href={routes.home()}>
         <Button className="mt-6">Go to homepage</Button>
       </Link>
+    </div>
+  );
+};
+
+export const LoadingData = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center border">
+      <Loader2 className="mr-2 h-12 w-12 animate-spin" />
+      <p className="mt-10 font-mono text-lg font-bold">
+        🪄 Some last minute magic...
+      </p>
     </div>
   );
 };
