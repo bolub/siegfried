fe415694a6db7aa44ea5a4d731ebfc69126fdfb0
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-17 00:54:05 +0300

    remove old function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,22 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function formatDate(date: Date): string {
-  const day = date.getDate();
-  const month = date.toLocaleString("default", { month: "long" });
-  const year = date.getFullYear();
-
-  let daySuffix: string;
-  if (day === 1 || day === 21 || day === 31) {
-    daySuffix = "st";
-  } else if (day === 2 || day === 22) {
-    daySuffix = "nd";
-  } else if (day === 3 || day === 23) {
-    daySuffix = "rd";
-  } else {
-    daySuffix = "th";
-  }
-
-  return `${day}${daySuffix} ${month} ${year}`;
-}
