d06be78ed577ecd09f180a9a7f977c2735609e12
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-16 16:47:49 +0300

    Example to test event emitter

--- src/server/modules/example/router.ts
+++ src/server/modules/example/router.ts
@@ -7,6 +7,7 @@ import {
 import { PdfService } from "@/server/modules/pdf-service/impl";
 import { EmailService } from "@/server/modules/email-service/impl";
 import ContractRequest from "@/emails/ContractRequest";
+import { EventService } from "../event-service/impl";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -40,5 +41,12 @@ export const exampleRouter = createTRPCRouter({
       html: "<h1>Installation</h1><p>Playwright Test was created specifically to accommodate the needs of end-to-end testing. Playwright supports all modern rendering engines including Chromium, WebKit, and Firefox. Test on Windows, Linux, and macOS, locally or on CI, headless or headed with native mobile emulation of Google Chrome for Android and Mobile Safari.</p><p><strong>You will learn</strong></p><ul><li><p>How to install Playwright</p></li><li><p>What's Installed</p></li><li><p>How to run the example test</p></li><li><p>How to open the HTML test report</p></li></ul>",
     });
     return resp;
+  }),
+
+  testEmitEvents: publicProcedure.mutation(() => {
+    EventService.Emitter.emit("CONTRACT_CREATED", {
+      contractId: "cllcbgdux0007v0uxhw330ixk",
+      userId: "cllcbgdux0007v0uxhw330ixk",
+    });
   }),
 });
