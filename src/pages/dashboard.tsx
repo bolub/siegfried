c6267c5f9832e71cace07a3a95aad6e56c15de59
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-19 21:40:06 +0300

    remove data fetching from serverSideProps

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,37 +1,9 @@
 import { DashboardPage } from "@/containers/dashboard-page/DashboardPage";
 import { routes } from "@/routes";
 import { getServerAuthSession } from "@/server/auth";
-import { prisma } from "@/server/db";
-import { ContractService } from "@/server/modules/contract-service/impl";
-import { type Activity, type ContractRecipient } from "@prisma/client";
-import {
-  type InferGetServerSidePropsType,
-  type GetServerSideProps,
-  type GetServerSidePropsContext,
-} from "next";
+import { type GetServerSidePropsContext } from "next";
 
-type CustomActivity = Activity & {
-  recipient: Pick<ContractRecipient, "name"> | null;
-  contract: {
-    name: string | null;
-    user: {
-      name: string | null;
-    };
-  };
-};
-
-export type DashboardType = {
-  stats: {
-    total: number;
-    signed: number;
-    pending: number;
-  };
-  recentActivities: CustomActivity[];
-};
-
-export const getServerSideProps: GetServerSideProps<DashboardType> = async (
-  ctx: GetServerSidePropsContext
-) => {
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const session = await getServerAuthSession(ctx);
   const userId = session?.user?.id;
 
@@ -44,48 +16,11 @@ export const getServerSideProps: GetServerSideProps<DashboardType> = async (
     };
   }
 
-  const stats = await ContractService.stats({
-    userId,
-  });
-
-  const recentActivities = await prisma.activity.findMany({
-    take: 5,
-    where: {
-      userId,
-    },
-    orderBy: {
-      timestamp: "desc",
-    },
-    include: {
-      recipient: {
-        select: {
-          name: true,
-        },
-      },
-      contract: {
-        select: {
-          name: true,
-          user: {
-            select: {
-              name: true,
-            },
-          },
-        },
-      },
-    },
-  });
-
   return {
-    props: {
-      stats,
-      recentActivities,
-    },
+    props: {},
   };
 };
 
-export default function Dashboard({
-  stats,
-  recentActivities,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return <DashboardPage stats={stats} recentActivities={recentActivities} />;
+export default function Dashboard() {
+  return <DashboardPage />;
 }
