0aa8fcb2ab13eddd2a3479bbbd79e6eb524e4c66
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-19 22:06:36 +0300

    remove contracts fetch on server

--- src/pages/contracts/index.tsx
+++ src/pages/contracts/index.tsx
@@ -1,17 +1,9 @@
 import { ContractsPage } from "@/containers/contracts-list/Contracts";
 import { routes } from "@/routes";
 import { getServerAuthSession } from "@/server/auth";
-import { prisma } from "@/server/db";
-import { type Contract } from "@prisma/client";
-import {
-  type GetServerSidePropsContext,
-  type GetServerSideProps,
-  type InferGetServerSidePropsType,
-} from "next";
+import { type GetServerSidePropsContext } from "next";
 
-export const getServerSideProps: GetServerSideProps<{
-  contracts: Contract[];
-}> = async (ctx: GetServerSidePropsContext) => {
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const session = await getServerAuthSession(ctx);
   const userId = session?.user?.id;
 
@@ -24,27 +16,11 @@ export const getServerSideProps: GetServerSideProps<{
     };
   }
 
-  const contracts = await prisma.contract.findMany({
-    where: {
-      userId,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-    include: {
-      recipients: true,
-    },
-  });
-
   return {
-    props: {
-      contracts,
-    },
+    props: {},
   };
 };
 
-export default function Contracts({
-  contracts,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return <ContractsPage contracts={contracts} />;
+export default function Contracts() {
+  return <ContractsPage />;
 }
