69fc7c319e0455f5a11facf7c7ad26bf82b41dba
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-19 21:40:40 +0300

    create activty service interface

---
+++ src/server/modules/activity-service/interface.ts
@@ -0,0 +1,19 @@
+import { type Activity, type ContractRecipient } from "@prisma/client";
+
+type CustomActivity = Activity & {
+  recipient: Pick<ContractRecipient, "name"> | null;
+  contract: {
+    name: string | null;
+    user: {
+      name: string | null;
+    };
+  };
+};
+
+export interface ActivityServiceType {
+  recent: (args: {
+    user: {
+      id: string;
+    };
+  }) => Promise<CustomActivity[]>;
+}
