4b0a79d1ddac7f31bd8d2be5397390c79ceda486
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-19 21:40:13 +0300

    Add activity router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,11 +2,13 @@ import { exampleRouter } from "@/server/modules/example/router";
 import { createTRPCRouter } from "@/server/api/trpc";
 import { contractServiceRouter } from "@/server/modules/contract-service/router";
 import { pdfServiceRouter } from "@/server/modules/pdf-service/router";
+import { activityServiceRouter } from "@/server/modules/activity-service/router";
 
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   contract: contractServiceRouter,
   pdf: pdfServiceRouter,
+  activity: activityServiceRouter,
 });
 
 // export type definition of API
