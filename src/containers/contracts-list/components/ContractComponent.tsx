d1e23223cbef2e90ae4b50f10b6f18e0539d9902
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-21 12:34:45 +0300

    Add Link component to point to new page

--- src/containers/contracts-list/components/ContractComponent.tsx
+++ src/containers/contracts-list/components/ContractComponent.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/components/ui/tooltip";
 import { type ContractForList } from "@/server/modules/contract-service/interface";
 import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
+import { routes } from "@/routes";
 
 export const ContractComponentShimmer = () => {
   return (
@@ -44,42 +46,50 @@ export const ContractComponent = ({
   contract: ContractForList;
 }) => {
   return (
-    <div className="border-[rgba(0, 0, 0, 0.08)] w-full rounded-lg border bg-white">
-      {/* top */}
-      <div className="mt-6 flex items-center space-x-[20px] px-8">
-        <div className="border-[rgba(0, 0, 0, 0.08)] flex h-[50px] w-[50px] items-center justify-center rounded-lg border-[1.5px] bg-[#f9fafb]">
-          <PencilLine className="h-6 w-6" />
+    <Link
+      href={
+        contract.status === "PENDING"
+          ? routes.contracts.edit(contract.id)
+          : routes.contracts.all()
+      }
+    >
+      <div className="border-[rgba(0, 0, 0, 0.08)] w-full rounded-lg border bg-white">
+        {/* top */}
+        <div className="mt-6 flex items-center space-x-[20px] px-8">
+          <div className="border-[rgba(0, 0, 0, 0.08)] flex h-[50px] w-[50px] items-center justify-center rounded-lg border-[1.5px] bg-[#f9fafb]">
+            <PencilLine className="h-6 w-6" />
+          </div>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <h3 className="truncate text-lg font-semibold">
+                  {contract.name}
+                </h3>
+              </TooltipTrigger>
+              <TooltipContent>{contract.name}</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
 
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <h3 className="truncate text-lg font-semibold">
-                {contract.name}
-              </h3>
-            </TooltipTrigger>
-            <TooltipContent>{contract.name}</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        {/* bottom */}
+        <div className="mt-8 flex flex-col space-y-[20px] px-8 pb-9 text-sm">
+          <div className="flex w-full items-center justify-between">
+            <span className="font-medium text-[#667085]">Status</span>
+            <Badge
+              variant={contract.status === "PENDING" ? "secondary" : "success"}
+              className="text-sm"
+            >
+              {contract.status}
+            </Badge>
+          </div>
+
+          <div className="flex w-full items-center justify-between">
+            <span className="font-medium text-[#667085]">Created on</span>
+            <p className="font-medium">{formatDate(contract.createdAt)}</p>
+          </div>
+        </div>
       </div>
-
-      {/* bottom */}
-      <div className="mt-8 flex flex-col space-y-[20px] px-8 pb-9 text-sm">
-        <div className="flex w-full items-center justify-between">
-          <span className="font-medium text-[#667085]">Status</span>
-          <Badge
-            variant={contract.status === "PENDING" ? "secondary" : "success"}
-            className="text-sm"
-          >
-            {contract.status}
-          </Badge>
-        </div>
-
-        <div className="flex w-full items-center justify-between">
-          <span className="font-medium text-[#667085]">Created on</span>
-          <p className="font-medium">{formatDate(contract.createdAt)}</p>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 };
