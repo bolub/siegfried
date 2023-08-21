5fbbfcafd7c1370856a5ad209b516e03013beb21
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-21 12:34:56 +0300

    Create edit contract component

---
+++ src/containers/contracts-edit/EditContract.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { ContractTitleEditor } from "@/containers/contract-new/components/ContractTitleEditor";
+import { ContractEditor } from "@/containers/contract-new/components/ContractEditor/ContractEditor";
+import { ContractSigners } from "@/containers/contract-new/components/ContractSigners/ContractSigners";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { useLeavePageConfirm } from "@/hooks/useLeavePageConfirm";
+import { type ContractFormData } from "@/containers/contract-new/components/ContractSigners/interface";
+import { api } from "@/utils/api";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { useRouter } from "next/router";
+import { routes } from "@/routes";
+import { type SingleContractType } from "@/pages/contracts/edit/[id]";
+import { ContractSignersFooter } from "@/containers/contract-new/components/ContractSigners/ContractSignersFooter";
+
+export const EditContractPage = ({
+  contract,
+}: {
+  contract?: SingleContractType | null;
+}) => {
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const {
+    mutate: updateContract,
+    isLoading: updateContractLoading,
+    isSuccess: hideConfirmationWhenContractIsCreatedSuccessfully,
+  } = api.contract.update.useMutation({
+    onSuccess() {
+      toast({
+        title: "Success",
+        description: "Contract updated successfully",
+      });
+
+      void router.push(routes.contracts.all());
+    },
+    onError() {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+        action: <ToastAction altText="Try again">Try again</ToastAction>,
+      });
+    },
+  });
+
+  useLeavePageConfirm({
+    message:
+      "Are you sure you want to leave this page? you still have unsaved changes",
+    isConfirm: !hideConfirmationWhenContractIsCreatedSuccessfully,
+  });
+
+  const onSubmit: SubmitHandler<ContractFormData> = (data) => {
+    updateContract({
+      id: contract?.id || "",
+      contractName: data.contractName,
+      contractContent: data.contractContent,
+    });
+  };
+
+  const { handleSubmit, register, control } = useForm<ContractFormData>();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <ContractTitleEditor contract={contract} register={register} />
+      <ContractEditor contract={contract} control={control} />
+      <ContractSigners
+        contract={contract}
+        register={register}
+        isLoading={updateContractLoading}
+        disabled
+      >
+        <ContractSignersFooter
+          isLoading={updateContractLoading}
+          action="Update Contract"
+          disabled={contract?.status === "SIGNED"}
+        />
+      </ContractSigners>
+    </form>
+  );
+};
