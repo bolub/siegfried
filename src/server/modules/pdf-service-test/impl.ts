9055e0f74b24d4a64e3e111ded69e414437c882e
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-21 17:45:33 +0300

    Test again

--- src/server/modules/pdf-service-test/impl.ts
+++ src/server/modules/pdf-service-test/impl.ts
@@ -1,11 +1,23 @@
-import puppeteer from "puppeteer";
 import path from "path";
 import { type PdfServiceTypeTest } from "./interface";
+import chromium from "chrome-aws-lambda";
+import playwright from "playwright-core";
 
 export const generatePdf: PdfServiceTypeTest["generatePdf"] = async ({
   html,
 }) => {
-  const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch();
+
+  // Start Playwright with the dynamic chrome-aws-lambda args
+  const browser = await playwright.chromium.launch({
+    args: chromium.args,
+    executablePath:
+      process.env.NODE_ENV !== "development"
+        ? await chromium.executablePath
+        : "/usr/bin/chromium",
+    headless: process.env.NODE_ENV !== "development" ? chromium.headless : true,
+  });
+
   const page = await browser.newPage();
   await page.setContent(html);
 
