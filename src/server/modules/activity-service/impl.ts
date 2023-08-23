cc9005af8419f335c11d24b0a867b7a14e278b20
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-23 19:23:27 +0300

    Create api route to get activity by contract

--- src/server/modules/activity-service/impl.ts
+++ src/server/modules/activity-service/impl.ts
@@ -30,6 +30,39 @@ const recent: ActivityServiceType["recent"] = async ({ user }) => {
   });
 };
 
+const byContract: ActivityServiceType["byContract"] = async ({
+  user,
+  contractId,
+}) => {
+  return await prisma.activity.findMany({
+    where: {
+      userId: user.id,
+      contractId,
+    },
+    orderBy: {
+      timestamp: "desc",
+    },
+    include: {
+      recipient: {
+        select: {
+          name: true,
+        },
+      },
+      contract: {
+        select: {
+          name: true,
+          user: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+};
+
 export const ActivityService: ActivityServiceType = {
   recent,
+  byContract,
 };
