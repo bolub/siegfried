77b7c8e4d08311e0a494e4128b334713faa38b3c
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-24 10:22:36 +0300

    remove unused

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,18 +1,16 @@
 import { cn } from "@/lib/utils";
 import React, { type ComponentProps, type FC } from "react";
+import LogoBlack from "public/logo.svg?url";
+import Image from "next/image";
 
 export const Logo: FC<ComponentProps<"span">> = (props) => {
   const { className, ...otherSpanProps } = props;
 
   return (
-    <span
-      className={cn([
-        "font-mono text-base font-bold uppercase text-primary-100",
-        className,
-      ])}
-      {...otherSpanProps}
-    >
-      Siegfried
-    </span>
+    <>
+      <span className={cn([className])} {...otherSpanProps}>
+        <Image src={LogoBlack} alt="Siegfried" className="w-[88px]" />
+      </span>
+    </>
   );
 };
