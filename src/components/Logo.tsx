b5243c0519cb32ef5d030c46769ba04fbcb877f9
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-24 12:39:29 +0300

    Add logo variant

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,14 +3,32 @@ import React, { type ComponentProps, type FC } from "react";
 import LogoBlack from "public/logo.svg?url";
 import Image from "next/image";
 
-export const Logo: FC<ComponentProps<"span">> = (props) => {
-  const { className, ...otherSpanProps } = props;
+export const Logo: FC<
+  ComponentProps<"span"> & {
+    type?: "text" | "logo";
+  }
+> = (props) => {
+  const { className, type = "logo", ...otherSpanProps } = props;
 
-  return (
-    <>
-      <span className={cn([className])} {...otherSpanProps}>
-        <Image src={LogoBlack} alt="Siegfried" className="w-[88px]" />
+  if (type === "text") {
+    return (
+      <span
+        className={cn([
+          "text-primary-100 font-mono text-base font-bold uppercase",
+          className,
+        ])}
+        {...otherSpanProps}
+      >
+        Siegfried
       </span>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <span className={cn([className])} {...otherSpanProps}>
+          <Image src={LogoBlack} alt="Siegfried" className="w-[88px]" />
+        </span>
+      </>
+    );
+  }
 };
