f93a7693ca4432b7972d7a6affc56dc5ccf8fb17
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-25 13:35:04 +0300

    Update seo content

--- utils/seo.ts
+++ utils/seo.ts
@@ -1,4 +1,10 @@
+import { type NextSeoProps } from "next-seo";
+
 export const seoConfig = {
   title: "Siegfried",
-  description: "Siegfried",
-} as const;
+  description:
+    "Easily create personalized contracts and send them for quick and secure electronic signatures.",
+  openGraph: {
+    url: "https://siegfried.dev",
+  },
+} as NextSeoProps;
