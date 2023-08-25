109136d62996d9fadc322297b7db620933938116
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-25 13:38:43 +0300

    Add image url

--- utils/seo.ts
+++ utils/seo.ts
@@ -1,10 +1,19 @@
 import { type NextSeoProps } from "next-seo";
 
+// https://www.siegfried.dev/og-image.png
+
 export const seoConfig = {
   title: "Siegfried",
   description:
     "Easily create personalized contracts and send them for quick and secure electronic signatures.",
   openGraph: {
     url: "https://siegfried.dev",
+    images: [
+      {
+        url: "https://www.siegfried.dev/og-image.png",
+        width: 1200,
+        height: 600,
+      },
+    ],
   },
 } as NextSeoProps;
