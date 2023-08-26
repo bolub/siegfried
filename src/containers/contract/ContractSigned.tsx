5cc6c19f966355148f1c5c3faf5f90183feb286c
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-08-26 08:15:03 +0300

    center logo

--- src/containers/contract/ContractSigned.tsx
+++ src/containers/contract/ContractSigned.tsx
@@ -37,7 +37,7 @@ export const ContractSignedPageInner = ({
     <>
       <main className="container flex h-screen w-full max-w-2xl">
         <div className="m-auto w-full flex-col text-center">
-          <Logo />
+          <Logo className="mx-auto" />
 
           <h1 className="mt-24 font-mono text-3xl font-bold leading-[50px]">
             Thank you for signing {contract.name}
